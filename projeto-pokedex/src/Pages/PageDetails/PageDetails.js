1c2520d3a8d8c8e2528ec01a5016cda6dc2c9911
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-06-15 18:39:22 -0300

    puxando detalhes fixos da API

--- projeto-pokedex/src/Pages/PageDetails/PageDetails.js
+++ projeto-pokedex/src/Pages/PageDetails/PageDetails.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { goBack } from '../../Route/coordinator';
 import { useHistory } from 'react-router';
 import { Header, ContainerGrid, ImgFront, ImgBack, Stats, Type, Moves } from './styled';
+import axios from 'axios';
 
 const PageDetails = () => {
+  const pokeURL = "https://pokeapi.co/api/v2/pokemon"
   const history = useHistory()
 
+  const [pokemon, setPokemon] = useState({})
+  useEffect(() => {
+    const getPokemon = () => {
+      axios.get(`${pokeURL}/pikachu`)
+        .then((res) => setPokemon(res.data))
+        .catch((err) => console.log(err));
+    };
+    getPokemon();
+  }, [setPokemon, pokeURL]);
+
+  console.log(pokemon)
+ 
   return (
     <div>
       <Header>
@@ -18,22 +32,52 @@ const PageDetails = () => {
       <ContainerGrid>
         <ImgFront>
           Imagem frontal
+          {pokemon.sprites && pokemon.sprites.front_default ? (
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />  ) : (
+          <p>Loading...</p>
+        )}
         </ImgFront>
 
         <ImgBack>
           Imagem de costas
+          {pokemon.sprites && pokemon.sprites.back_default ? (
+        <img src={pokemon.sprites.back_default} alt={pokemon.name} />  ) : (
+          <p>Loading...</p>
+        )}
         </ImgBack>
 
         <Stats>
           Stats
+          {pokemon.stats ? (pokemon.stats.map((stat) => {
+            return (
+              <p>
+                <strong>{stat.stat.name}:</strong>
+                {stat.base_stat}
+              </p>
+          )
+          })) : (<p>Loading...</p>)}
         </Stats>
 
         <Type>
           Type
+          {pokemon.types ? (pokemon.types.map((type) => {
+            return (
+              <p>
+                <strong>{type.type.name}</strong>
+              </p>
+          )
+          })) : (<p>Loading...</p>)}
         </Type>
 
         <Moves>
           Moves
+          {pokemon.moves ? (pokemon.moves.map((move) => {
+            return (
+              <p>
+                <strong>{move.move.name}</strong>
+              </p>
+          )
+          })) : (<p>Loading...</p>)}
         </Moves>
 
       </ContainerGrid>
